2dad30c7910fac1c197b7a92067ea87ac3418251
manishjanky <manishjanky@gmail.com>
2022-07-09 02:05:01 +0530

    Eslint

--- projects/ngx-select-dropdown/src/lib/pipes/limit-to.pipe.ts
+++ projects/ngx-select-dropdown/src/lib/pipes/limit-to.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'limitTo'
 })
 export class LimitToPipe implements PipeTransform {
-  public transform(array: any[], itemsCount: number, startIndex: number = 0) {
+  public transform(array: any[], itemsCount: number, startIndex = 0) {
     if (!Array.isArray(array) || itemsCount === 0) {
       return array;
     }
