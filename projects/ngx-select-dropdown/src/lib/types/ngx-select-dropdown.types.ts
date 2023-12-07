f72d6c6f2dae42ccad8d3b609945f635f524c0ba
manishjanky <manishjanky@gmail.com>
2023-12-07 20:30:06 +0530

    #287

--- projects/ngx-select-dropdown/src/lib/types/ngx-select-dropdown.types.ts
+++ projects/ngx-select-dropdown/src/lib/types/ngx-select-dropdown.types.ts
@@ -1,17 +1,17 @@
 export interface NgxDropdownConfig {
   displayFn?: any;
-  displayKey: string;
-  search: boolean;
-  height: string;
-  placeholder: string;
-  customComparator: (a: any, b: any) => number;
-  limitTo: number;
-  moreText: string;
-  noResultsFound: string;
-  searchPlaceholder: string;
-  searchOnKey: string;
-  clearOnSelection: boolean;
-  inputDirection: string;
+  displayKey?: string;
+  search?: boolean;
+  height?: string;
+  placeholder?: string;
+  customComparator?: (a: any, b: any) => number;
+  limitTo?: number;
+  moreText?: string;
+  noResultsFound?: string;
+  searchPlaceholder?: string;
+  searchOnKey?: string;
+  clearOnSelection?: boolean;
+  inputDirection?: string;
   selectAllLabel?: string;
   enableSelectAll?: boolean;
 }
